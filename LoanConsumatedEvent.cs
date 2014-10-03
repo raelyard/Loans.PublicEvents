d985ed4221476814a4665949e055638921a69621
Dave Rael <dave@raelyard.com>
2014-10-03 12:47:19 -0600

    Added event for indicating a loan has been consumated

---
+++ LoanConsumatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MediaLoanLibrary.Loans.PublicEvents
+{
+    public interface LoanConsumatedEvent
+    {
+        int LoanId { get; set; }
+        DateTime DueDate { get; set; }
+    }
+}
