bfdef666c285667f5d03c1560670e76a342c1329
Dave Rael <dave@raelyard.com>
2014-10-04 11:14:16 -0600

    Added GitFlowTask package for easy versioning
    so that the output package will have a sensible version for using as a
    dependency for this and other services

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c4dbacc4-0e98-4334-ace8-0072e0990076")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
