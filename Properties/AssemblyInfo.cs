61562808f17d5739279b60abf19c4dda4f668054
Dave Rael <dave@raelyard.com>
2014-10-10 05:16:02 -0600

    Fixed fault in assembly name in assembly info
    had a wrong default assembly name that needed to be fixed to properly
    reflect in the assembly meta-informaation and in the nuget package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("Loans.PublicEvents")]
 [assembly: AssemblyDescription("Events that serve as contracts for what is published by the Loans Subdomain")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("Loans.PublicEvents")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
